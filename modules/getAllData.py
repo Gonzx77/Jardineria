f8f5ad0b4bb6b510f56be8cb0fb6bc201d32b39a
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-03-19 07:00:13 -0500

    Se actualizaron todos los storage ".json"

--- modules/getAllData.py
+++ modules/getAllData.py
@@ -3,34 +3,34 @@ import requests
 
 
 def DetallePed():
-    peticion = requests.get("http://172.16.100.138:5508/")
+    peticion = requests.get("http://172.16.100.138:5508/detalle")
     data = peticion.json()
     return data
 def Gama():
-    peticion = requests.get("http://172.16.100.138:5507/")
+    peticion = requests.get("http://172.16.100.138:5507/gama")
     data = peticion.json()
     return data
 def Producto():
-    peticion = requests.get("http://172.16.100.138:5506/")
+    peticion = requests.get("http://172.16.100.138:5506/producto")
     data = peticion.json()
     return data
 def Pedido():
-    peticion = requests.get("http://172.16.100.138:5505/")
+    peticion = requests.get("http://172.16.100.138:5505/pedido")
     data = peticion.json()
     return data
 def Oficina():
-    peticion = requests.get("http://172.16.100.138:5504/")
+    peticion = requests.get("http://172.16.100.138:5504/oficina")
     data = peticion.json()
     return data
 def Cliente():
-    peticion = requests.get("http://172.16.100.138:5503/")
+    peticion = requests.get("http://172.16.100.138:5503/cliente")
     data = peticion.json()
     return data
 def Empleado():
-    peticion = requests.get("http://172.16.100.138:5502/")
+    peticion = requests.get("http://172.16.100.138:5502/empleado")
     data = peticion.json()
     return data
 def Pago():
-    peticion = requests.get("http://172.16.100.138:5501/")
+    peticion = requests.get("http://172.16.100.138:5501/pago")
     data = peticion.json()
     return data
